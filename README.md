81295bd4c3b0680d8d5e150747aa8a4dd23f82cf
zique1104 <diyueyu0413@163.com>
2018-07-10 15:41:37 +0800

    提交数据

---
+++ README.md
@@ -0,0 +1,1 @@
+##代码介绍： http://blog.csdn.net/xiazai353503200/article/details/79390879
---
+++ src/main/java/com/example/game/domain/pojo/TestPojo.java
@@ -0,0 +1,10 @@
+package com.example.game.domain.pojo;
+
+
+public class TestPojo{
+	private String id;
+	private String name;
+	private String sex;
+	
+	
+}
